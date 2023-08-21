675bf38b0117f55f084e27dc8d65096259810777
Marius Storhaug <marstor@hotmail.com>
2023-08-22 01:14:54 +0200

    test

--- scripts/Publish-Module.ps1
+++ scripts/Publish-Module.ps1
@@ -11,5 +11,14 @@ $SRCPath = Get-Item -Path .\src\ | Select-Object -ExpandProperty FullName
 $env:PSModulePath += ":$SRCPath"
 $env:PSModulePath -Split ':'
 
+$Manifest = Invoke-Expression (Get-Content -Path 'C:\Repos\MariusStorhaug\PATH\src\PATH\PATH.psd1' -Raw)
+$Manifest.RequiredModules | ForEach-Object {
+    $Module = Get-Module -Name $_.ModuleName -ListAvailable
+    if ($Module) {
+        Write-Verbose "Importing module $($_.ModuleName) from $($Module.Path)"
+        Import-Module -Name $Module.Path -Verbose
+    }
+}
+
 .\scripts\Set-ModuleVersion.ps1 -ModuleName $ModuleName -Verbose
 Publish-Module -Path "src/$ModuleName" -NuGetApiKey $APIKey -Verbose
