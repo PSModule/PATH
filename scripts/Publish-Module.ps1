77785a8cd859cdb5a66567af3b9cf245da0d817b
Marius Storhaug <marstor@hotmail.com>
2023-08-22 01:28:25 +0200

    fix

--- scripts/Publish-Module.ps1
+++ scripts/Publish-Module.ps1
@@ -11,13 +11,21 @@ $SRCPath = Get-Item -Path .\src\ | Select-Object -ExpandProperty FullName
 $env:PSModulePath += ":$SRCPath"
 $env:PSModulePath -Split ':'
 
-$Manifest = Invoke-Expression (Get-Content -Path 'C:\Repos\MariusStorhaug\PATH\src\PATH\PATH.psd1' -Raw)
-$Manifest.RequiredModules | ForEach-Object {
-    $Module = Get-Module -Name $_.ModuleName -ListAvailable
-    if ($Module) {
-        Write-Verbose "Importing module $($_.ModuleName) from $($Module.Path)"
-        Import-Module -Name $Module.Path -Verbose
+$Manifest = Invoke-Expression (Get-Content -Path "src/$ModuleName/$ModuleName.psd1" -Raw)
+foreach ($Module in $Manifest.RequiredModules) {
+    $InstallParams = @{}
+
+    if ($Module -is [string]) {
+        $InstallParams.Name = $Module
+    } else {
+        $InstallParams.Name = $Module.ModuleName
+        $InstallParams.MinimumVersion = $Module.ModuleVersion
+        $InstallParams.RequiredVersion = $Module.RequiredVersion
     }
+    $InstallParams.Verbose = $true
+    $InstallParams.Force = $true
+
+    Install-Module @InstallParams
 }
 
 .\scripts\Set-ModuleVersion.ps1 -ModuleName $ModuleName -Verbose
