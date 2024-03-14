340f99c45a87c023122d9499a1229f18135fb76a
Marius Storhaug <marstor@hotmail.com>
2024-03-14 19:03:08 +0100

    🩹 [Patch]: Add tests for PATH (#5)
    ## Description
    
    - Add tests for PATH
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ tests/PATH.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'PATH' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'PATH' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'PATH' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be imported' {
+            { Import-Module -Name 'PATH' } | Should -Not -Throw
+        }
+    }
+}
