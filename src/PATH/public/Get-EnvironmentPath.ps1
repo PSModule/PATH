b695a12928f42f7ba36c0bf1f45dcabdd0dafcd7
Marius Storhaug <marstor@hotmail.com>
2024-03-14 18:43:44 +0100

    Initial version (#4)
    ## Description
    
    - Initial version
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/PATH/public/Get-EnvironmentPath.ps1
@@ -0,0 +1,52 @@
+﻿function Get-EnvironmentPath {
+    <#
+    .SYNOPSIS
+    Get the PATH environment variable.
+
+    .DESCRIPTION
+    Get the PATH environment variable for the current user or all users.
+
+    .EXAMPLE
+    Get-EnvironmentPath -Scope CurrentUser
+
+    Get the PATH environment variable for the current user.
+
+    .EXAMPLE
+    Get-EnvironmentPath -Scope AllUsers -AsArray
+
+    Get the PATH environment variable for the current user as an array.
+    #>
+    [OutputType([string[]], ParameterSetName = 'AsArray')]
+    [OutputType([string], ParameterSetName = 'AsString')]
+    [CmdletBinding(DefaultParameterSetName = 'AsString')]
+    param(
+        # The scope of the environment variable.
+        [Parameter()]
+        [ValidateSet('AllUsers', 'CurrentUser')]
+        [string] $Scope = 'CurrentUser',
+
+        # Return the environment variable as an array.
+        [Parameter(ParameterSetName = 'AsArray')]
+        [switch] $AsArray
+    )
+
+    $target = if ($Scope -eq 'CurrentUser') {
+        [System.EnvironmentVariableTarget]::User
+    } else {
+        [System.EnvironmentVariableTarget]::Machine
+    }
+
+    $environmentPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::$target)
+    if (-not $AsArray) {
+        return $environmentPath
+    }
+    if ($IsWindows) {
+        $pathSeparator = ';'
+    } else {
+        $pathSeparator = ':'
+    }
+    $environmentPath = $environmentPath.Split($pathSeparator)
+    $environmentPath = $environmentPath | Sort-Object
+
+    return $environmentPath
+}
