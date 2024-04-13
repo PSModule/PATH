8e988c3b2ae9d5d8defd35a7dfa569ac48206091
Marius Storhaug <marstor@hotmail.com>
2024-04-14 00:24:30 +0200

    Remove module import test

--- tests/PATH.Tests.ps1
+++ tests/PATH.Tests.ps1
@@ -8,16 +8,6 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'PATH' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'PATH' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'PATH' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be imported' {
-            { Import-Module -Name 'PATH' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
-
     Context 'Function: Get-EnvironemntPath' {
         Context 'CurrentUser' {
             It 'Should not throw' {
