0436d6fa3b7b7a9afaa667ebbeb2d5877ef88b3e
Marius Storhaug <marstor@hotmail.com>
2024-11-01 23:16:44 +0100

    Remove redundant parameter from tests

--- tests/PATH.Tests.ps1
+++ tests/PATH.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'PATH' {
+﻿Describe 'PATH' {
     Context 'Function: Get-EnvironemntPath' {
         Context 'CurrentUser' {
             It 'Should not throw' {
